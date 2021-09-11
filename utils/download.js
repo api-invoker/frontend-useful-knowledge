a94b44eacda666244f7ca2ec1989e5bf72da643b
web-lrh <46252189+web-lrh@users.noreply.github.com>
2021-09-11 08:41:05 +0800

    Update download.js

--- utils/download.js
+++ utils/download.js
@@ -1,1 +1,11 @@
+export default function handleDownload() {
+  const a = document.createElement('a');
+  const blob = new Blob(服务器返回的二进制流);
+  const blobUrl = URL.createObjectURL(blob);
+  a.style.display = 'none';
+  a.download = filename;
+  a.href = blobUrl;
+  a.click();
+  URL.removeObjectURL(blobUrl);
+}
 
