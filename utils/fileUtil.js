a7da16376af4826a907791d1cf4bb964478b0bb4
web-lrh <46252189+web-lrh@users.noreply.github.com>
2021-09-14 20:25:23 +0800

    Update fileUtil.js

--- utils/fileUtil.js
+++ utils/fileUtil.js
@@ -6,7 +6,7 @@
  *                如果单位数组里面只指定了到了K(千字节)，同时文件大小大于M, 此方法的输出将还是显示成多少K.
  */
 function formatFileSize(size, pointLength, units) {
-  if(!size || isNaN(size)) {
+  if (!size || isNaN(size) || Number(size) === 0) {
     return "0B";
   }
   let unit;
